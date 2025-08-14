6a3848372f8c3c47d39d4c6ee90c272137417274
Akhil Pandey <akhilpandey494@gmail.com>
2025-08-14 12:30:48 +0530

    Lab 1

--- Lab-1.sql
+++ Lab-1.sql
@@ -26,3 +26,30 @@ salary DECIMAL (10,2) DEFAULT 50000
 )
 desc Doctor_Table
 ALTER TABLE Doctor_Table ADD CONSTRAINT chk_salary CHECK (salary>=30000)
+ALTER TABLE Doctor_Table
+RENAME COLUMN patient_id TO doctor_id;
+DESC Doctor_Table
+create table Appointment_table(
+  appointment_id Integer Primary Key,
+  patient_id Integer NOT NULL,
+  doctor_id Integer NOT NULL,
+  appointment_date Date NOT NULL,
+  status Varchar(20),
+  FOREIGN KEY (patient_id) REFERENCES Patient_Table(patientid),
+  FOREIGN KEY (doctor_id) REFERENCES Doctor_Table(doctor_id)
+)
+DESC Appointment_table
+ALTER TABLE Appointment_table ADD CONSTRAINT chk_status CHECK (status IN('Scheduled','Completed','Cancelled'))
+create table Bill_Table(
+ bill_id Integer Primary Key,
+ appointment_id Integer NOT NULL,
+ FOREIGN KEY (appointment_id) REFERENCES Appointment_table(appointment_id),
+ total_amount Decimal(10,2),
+ paid_amount Decimal(10,2),
+ payment_mode Varchar(20)
+)
+ Alter table Bill_Table Add Constraint chk_amount CHECK (paid_amount >= 0 AND paid_amount <=
+total_amount)
+ ALter table Bill_Table Add Constraint chk_pay CHECK (payment_mode IN('Cash','Card','UPI'))
+Desc Bill_Table
+Alter table Patient_table Add Address VARCHAR(200)
