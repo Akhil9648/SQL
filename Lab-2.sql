4ea77581c9819d4e91354af7d37321fdffc3e698
Akhil Pandey <akhilpandey494@gmail.com>
2025-08-22 14:30:28 +0530

    Lab-2.sql

--- Lab-2.sql
+++ Lab-2.sql
@@ -28,3 +28,51 @@ VALUES
 (70, 'Karan','General Surgery', '6754356754', '55000');
 SELECT * from Doctor_Table
 DESC Doctor_Table
+INSERT INTO Doctor_Table
+(doctor_id, doctor_name, specialization, contact_no, salary)
+VALUES
+(10, 'Abhay','General Surgery', '9876544343', '65000');
+DESC Appointment_table
+INSERT INTO Appointment_table
+(appointment_id, patient_id, doctor_id, APPOINTMENT_DATE, status)
+VALUES
+(12345, 100,10,DATE '2025-01-17' , 'Scheduled');
+INSERT INTO Appointment_table
+(appointment_id, patient_id, doctor_id, APPOINTMENT_DATE, status)
+VALUES
+(12346, 101,10,DATE '2025-02-20' , 'Completed');
+INSERT INTO Appointment_table
+(appointment_id, patient_id, doctor_id, APPOINTMENT_DATE, status)
+VALUES
+(32432, 100,20,DATE '2025-09-12' , 'Cancelled');
+INSERT INTO Appointment_table
+(appointment_id, patient_id, doctor_id, APPOINTMENT_DATE, status)
+VALUES
+(21232, 104,30,DATE '2025-04-11' , 'Scheduled');
+INSERT INTO Appointment_table
+(appointment_id, patient_id, doctor_id, APPOINTMENT_DATE, status)
+VALUES
+(51002, 104,20,DATE '2025-03-15' , 'Completed');
+SELECT * from Appointment_table
+DESC Bill_Table
+INSERT INTO Bill_table
+(bill_id, appointment_id, total_amount, paid_amount, payment_mode)
+VALUES
+(12, 12345,1000,900 , 'Cash');
+INSERT INTO Bill_table
+(bill_id, appointment_id, total_amount, paid_amount, payment_mode)
+VALUES
+(13, 32432,1200,1100 , 'Card');
+INSERT INTO Bill_table
+(bill_id, appointment_id, total_amount, paid_amount, payment_mode)
+VALUES
+(14, 21232,1200,1200 , 'UPI');
+INSERT INTO Bill_table
+(bill_id, appointment_id, total_amount, paid_amount, payment_mode)
+VALUES
+(15, 51002,800,800 , 'Cash');
+INSERT INTO Bill_table
+(bill_id, appointment_id, total_amount, paid_amount, payment_mode)
+VALUES
+(16, 12346,1000,950 , 'Cash');
+SELECT * from bill_table
