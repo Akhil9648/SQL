71918b06a9ddeafdd2bdc828840c9d31c0d703b1
Akhil Pandey <akhilpandey494@gmail.com>
2025-08-22 14:10:35 +0530

    Create Lab-2.sql

---
+++ Lab-2.sql
@@ -0,0 +1,30 @@
+INSERT INTO Doctor_Table
+(doctor_id, doctor_name, specialization, contact_no, salary)
+VALUES
+(10, 'Abhay','General Surgery', '9876544343', '65000');
+INSERT INTO Doctor_Table
+(doctor_id, doctor_name, specialization, contact_no, salary)
+VALUES
+(20, 'Gurav','Pediatrics', '9786767677', '75000');
+INSERT INTO Doctor_Table
+(doctor_id, doctor_name, specialization, contact_no, salary)
+VALUES
+(30, 'Esha','Nephrology', '9876545454', '68000');
+INSERT INTO Doctor_Table
+(doctor_id, doctor_name, specialization, contact_no, salary)
+VALUES
+(40, 'Sanjay','Cardiologist', '9878967899', '70000');
+INSERT INTO Doctor_Table
+(doctor_id, doctor_name, specialization, contact_no, salary)
+VALUES
+(50, 'Indresh','Pediatrics', '9954545454', '72000');
+INSERT INTO Doctor_Table
+(doctor_id, doctor_name, specialization, contact_no, salary)
+VALUES
+(60, 'Shashi','Cardiologist', '7865476546', '80000');
+INSERT INTO Doctor_Table
+(doctor_id, doctor_name, specialization, contact_no, salary)
+VALUES
+(70, 'Karan','General Surgery', '6754356754', '55000');
+SELECT * from Doctor_Table
+DESC Doctor_Table
