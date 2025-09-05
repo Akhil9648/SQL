64f9fbde15c37fb9fd1ad887111ab26bf5dd5811
Akhil Pandey <akhilpandey494@gmail.com>
2025-09-05 14:48:47 +0530

    Update Lab-4.sql

--- Lab-4.sql
+++ Lab-4.sql
@@ -1,3 +1,4 @@
+-- Like Operator in SQL
 SELECT * from Doctor_Table
 WHERE DOCTOR_NAME LIKE '%a'
 SELECT * from Doctor_Table
@@ -10,6 +11,7 @@ SELECT * from Doctor_Table
 WHERE SPECIALIZATION LIKE '%ist%'
 SELECT * from Patient_Table
 WHERE LAST_NAME LIKE 'T%i'
+-- Count Operator Used to Count the occurences of the Following key
 SELECT COUNT(*) from Patient_Table
 WHERE Address='Kanpur'
 SELECT COUNT(*) from Patient_Table
@@ -20,9 +22,11 @@ SELECT COUNT(*) from Patient_Table
 WHERE LAST_NAME='Sharma'
 SELECT COUNT(*) from Patient_Table
 WHERE Gender='F'
+-- Avg is used to find the average of the specific column
 SELECT AVG(salary) from Doctor_Table
 SELECT COUNT(*) from Doctor_Table
 WHERE SPECIALIZATION='Pediatrics'
+ -- Sum is used to Calculate the sum of all rows in the column
 SELECT SUM(total_amount) from Bill_Table
 SELECT COUNT(*) from Bill_Table
 WHERE payment_mode='Cash'
